ccfa81ef6a4f84601778a5a5e15fe373a865ce81
MariamKhuts <157484154+MariamKhuts@users.noreply.github.com>
2024-03-30 13:46:31 +0400

    add smth

---
+++ script.js
@@ -0,0 +1,33 @@
+let langs=document.querySelector(".langs"),
+   link=document.querySelectorAll("a"),
+   title=document.querySelector(".title"),
+   descr=document.querySelector(".description")
+
+
+
+let data = {
+
+    georgian:{  title:'Ჩვენი ამბავი',
+                  description:'',
+                  paragraph: ''
+
+    },
+
+
+    greek :{title:'',
+    description:'',
+    paragraph: '',
+
+    },
+    french : {title:'',
+    description:'',
+    paragraph: '',
+
+    },
+    arabic: {title:'',
+    description:'',
+    paragraph: '',
+
+    },
+
+}
